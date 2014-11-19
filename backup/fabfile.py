a6612a459cbbc93606133bc55417cf9203e2bce3
Matteo Ruina <matteo.ruina@gmail.com>
2014-11-19 16:10:16 -0300

    Create subdir for saving websites

--- backup/fabfile.py
+++ backup/fabfile.py
@@ -24,10 +24,18 @@ def wordpress_backup():
         wordpress_remote_folder = config.get(website, 'wordpress_remote_folder')
         backup_folder = config.get(website, 'backup_folder')
 
+        website_folder = os.path.join(backup_folder, website)
+        if not os.path.exists(website_folder):
+            os.mkdir(website_folder)
+            print("Directory {} created".format(website_folder))
+        else:
+            print("Warning: directory {} is not empty, I will save in it".format(website_folder))
+
         with settings(host_string=ssh_user + '@' + ssh_server, user=ssh_user, key_filename=ssh_key, warn_only=True):
             sudo('mysqldump -u root --password=' + mysql_root_password + ' ' + database + ' | gzip > /root/wp.db.gz')
             sudo('tar -zcvf /root/wp.tar.gz ' + wordpress_remote_folder)
-
             get(remote_path='/root/wp.db.gz', local_path=backup_folder)
             get(remote_path='/root/wp.tar.gz', local_path=backup_folder)
+            sudo('rm -rf /root/wp.db.gz')
+            sudo('rm -rf /root/wp.tar.gz')
             print(green('Ok: webiste {} backup complete!'.format(website)))
