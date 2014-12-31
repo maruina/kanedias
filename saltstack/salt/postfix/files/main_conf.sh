4e66bcff5303ef21943491c4edcac70544754a99
Matteo Ruina <matteo.ruina@gmail.com>
2014-12-30 21:34:51 -0300

    Postfix fixes

--- saltstack/salt/postfix/files/main_conf.sh
+++ saltstack/salt/postfix/files/main_conf.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "{{ salt['pillar.get']('postfix:fqdn') }}" > /etc/mailname
+postconf -e mydomain={{ salt['pillar.get']('postfix:fqdn') }}
+postconf -e mydestination="$myhostname, localhost, localhost.localdomain, localhost.$myhostname"
+postconf -e myhostname={{ salt['pillar.get']('postfix:fqdn') }}
 
 postconf -e virtual_transport=dovecot
 postconf -e dovecot_destination_recipient_limit=1
+
+postconf -e inet_protocols=all
+
+postconf -e smtpd_banner={{ salt['pillar.get']('postfix:fqdn') }}
--- saltstack/salt/postfix/files/smtp_authentication.sh
+++ saltstack/salt/postfix/files/smtp_authentication.sh
@@ -15,38 +15,38 @@ postconf -e smtpd_data_restrictions=reject_unauth_pipelining
 postconf -e smtpd_reject_unlisted_recipient=yes
 postconf -e smtpd_reject_unlisted_sender=yes
 postconf -e smtpd_helo_required=yes
-postconf -e smtpd_recipient_restrictions=" \
-  reject_unknown_sender_domain, \
-  reject_unknown_recipient_domain, \
-  reject_non_fqdn_recipient, \
-  reject_non_fqdn_sender, \
-  reject_unlisted_recipient, \
-  reject_unauth_destination, \
+postconf -e smtpd_recipient_restrictions="\
+  reject_unknown_sender_domain,\
+  reject_unknown_recipient_domain,\
+  reject_non_fqdn_recipient,\
+  reject_non_fqdn_sender,\
+  reject_unlisted_recipient,\
+  reject_unauth_destination,\
 {% if salt['pillar.get']('postfix:plugins:cluebringer') -%}
-  check_policy_service {{ salt['pillar.get']('postfix:plugins:cluebringer') }}, \
+  check_policy_service {{ salt['pillar.get']('postfix:plugins:cluebringer') }},\
 {%- endif %}
-  permit_mynetworks, \
+  permit_mynetworks,\
   permit_sasl_authenticated"
-postconf -e smtpd_sender_restrictions=" \
-  permit_mynetworks, \
-  reject_non_fqdn_sender, \
-  reject_unknown_sender_domain, \
-  reject_sender_login_mismatch, \
+postconf -e smtpd_sender_restrictions="\
+  permit_mynetworks,\
+  reject_non_fqdn_sender,\
+  reject_unknown_sender_domain,\
+  reject_sender_login_mismatch,\
   permit_sasl_authenticated"
-postconf -e smtpd_helo_restrictions=" \
-  permit_mynetworks, \
-  permit_sasl_authenticated,
-  reject_non_fqdn_helo_hostname, \
-  reject_invalid_helo_hostname, \
+postconf -e smtpd_helo_restrictions="\
+  permit_mynetworks,\
+  permit_sasl_authenticated,\
+  reject_non_fqdn_helo_hostname,\
+  reject_invalid_helo_hostname,\
   check_helo_access proxy:hash:/etc/postfix/helo_access"
-postconf -e smtpd_client_restrictions=" \
-  permit_mynetworks, \
-  permit_sasl_authenticated, \
-  reject_unauth_pipelining, \
-  reject_rbl_client cbl.abuseat.org, \
-  reject_rbl_client bl.spamcop.net, \
+postconf -e smtpd_client_restrictions="\
+  permit_mynetworks,\
+  permit_sasl_authenticated,\
+  reject_unauth_pipelining,\
+  reject_rbl_client cbl.abuseat.org,\
+  reject_rbl_client bl.spamcop.net,\
   reject_rbl_client zen.spamhaus.org"
 
 {% if salt['pillar.get']('postfix:plugins:cluebringer') -%}
-postconf -e smtpd_end_of_data_restrictions="{{ salt['pillar.get']('postfix:plugins:cluebringer') }}"
+postconf -e smtpd_end_of_data_restrictions="check_policy_service {{ salt['pillar.get']('postfix:plugins:cluebringer') }}"
 {%- endif %}
